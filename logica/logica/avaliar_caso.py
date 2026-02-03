79ed9c22f10903d0b6377b6d5dbb53c4f0e76cf1
paulohenriquedealmeida798014 <paulohenriquedealmeida798014@gmail.com>
2026-02-03 07:33:02 -0300

    Add initial avaliar_caso function (python)

---
+++ logica/logica/avaliar_caso.py
@@ -0,0 +1,17 @@
+# Função central de avaliação de caso
+# Versão inicial (esqueleto)
+
+def avaliar_caso(dados_avaliacao):
+    resultado = {
+        "status": "ok",
+        "alertas": [],
+        "bloqueios": [],
+        "texto_base": ""
+    }
+
+    # TODO: validar campos obrigatórios
+    # TODO: aplicar critérios de exclusão
+    # TODO: gerar alertas
+    # TODO: montar texto base
+
+    return resultado
