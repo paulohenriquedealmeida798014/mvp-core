1add9b8aebc0438f8f189d0440b1ee73dd8dfebe
paulohenriquedealmeida798014 <paulohenriquedealmeida798014@gmail.com>
2026-02-03 07:46:18 -0300

    Add exclusion rule: idade < 18

--- logica/logica/avaliar_caso.py
+++ logica/logica/avaliar_caso.py
@@ -15,6 +15,13 @@ def avaliar_caso(dados_avaliacao):
         resultado["bloqueios"].append("Campo obrigatório 'idade' não informado.")
         return resultado
 
+        # Regra 2: critério de exclusão simples - menor de 18 anos
+    if dados_avaliacao.get("idade") < 18:
+        resultado["status"] = "bloqueado"
+        resultado["bloqueios"].append("Idade menor que 18 anos não permitida.")
+        return resultado
+
+
     # TODO: outras validações obrigatórias
     # TODO: critérios de exclusão
     # TODO: gerar alertas
